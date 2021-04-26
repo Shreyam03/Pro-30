00de901b08a3f1a89d2410ab72ab88c45e5aa492
Shreyam03 <71756892+Shreyam03@users.noreply.github.com>
2021-04-26 19:16:22 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,31 @@
+class Box {
+  constructor(x,y,width,height,color) {
+    var options = {
+        isStatic: false
+    }
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    this.visibility = 255;
+    this.color = color;
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    if(this.body.speed<5){
+    push()
+    rectMode(CENTER);
+    fill(this.color);
+    rect(pos.x, pos.y, this.width, this.height);
+    pop();
+    }
+    else
+    {
+     World.remove(world,this.body);
+     push();
+     this.visibility=this.visibility-1;
+     pop();
+    }
+   
+  }
+};
